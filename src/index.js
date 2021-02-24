bab5481ef8126ba7ea2dd4339a05e6729f1d0638
leodhb <henrikleod@gmail.com>
2021-02-24 06:40:26 -0800

    aaaaaaa

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv/config');
+const app = express();
+
+const postsRouter = require('./routes/products-router');
+
+app.use(bodyParser.json());
+app.use('/posts', postsRouter);
+
+//COnn to db
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('[MONGODB] Conectado ao database'))
+    .catch(err => console.log('[MONGODB] Erro ao conectar ao database:', err));
+
+app.listen(3000, () => console.log('[API] Positivo e operante na porta 3000'));
+
---
+++ src/routes/products-router.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const Product = require('../models/product-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    const product = new Product({
+        name:  req.body.name,
+        image: req.body.image
+    });
+
+    try {
+        const savedProduct = await product.save();
+        res.json(savedProduct);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+});
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const singleProduct = await Product.findById(req.params.id);
+        res.json(singleProduct);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedProduct = await Product.remove({_id: req.params.id});
+        res.json(deletedProduct);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+});
+
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const updatedProduct = await Product.updateOne({_id: req.params.id}, { $set: {name: req.body.name}});
+        res.json(updatedProduct);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+})
+module.exports = router;
