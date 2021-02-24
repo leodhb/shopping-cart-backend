157fd917651b5230658caa61f1ad9c7a03c7623e
leodhb <henrikleod@gmail.com>
2021-02-24 17:52:14 -0300

    Separating controllers and route management

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,5 +2,62 @@ const Product = require('../models/product-model');
 
 
 const getAllProducts = async (req, res) => {
-    
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+}
+
+const insertProduct = async (req, res) => {
+    const product = new Product({
+        name:  req.body.name,
+        image: req.body.image,
+        sku: [req.body.sku]
+    });
+
+    try {
+        const savedProduct = await product.save();
+        res.json(savedProduct);
+        console.log(savedProduct)
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+}
+
+const getProductById = async (req, res) => {
+    try {
+        const singleProduct = await Product.findById(req.params.id);
+        res.json(singleProduct);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const deletedProduct = await Product.remove({_id: req.params.id});
+        res.json(deletedProduct);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+}
+
+const updateProduct = async (req, res) => {
+    try {
+        const updatedProduct = await Product.updateOne({_id: req.params.id}, { $set: {name: req.body.name, image: req.body.image, sku: [req.body.sku]}});
+        res.json(updatedProduct);
+    } catch (error) {
+        res.status(400).send({"error": error});
+    }
+}
+
+
+module.exports = {
+    getAllProducts,
+    insertProduct,
+    getProductById,
+    deleteProduct,
+    updateProduct
 }
