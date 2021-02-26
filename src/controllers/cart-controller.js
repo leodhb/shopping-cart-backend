9408d8b6d2b901c23f99f7e5d0f57c038c29da12
leodhb <henrikleod@gmail.com>
2021-02-26 17:45:42 -0300

    Refatorando alguns métodos do carrinho

--- src/controllers/cart-controller.js
+++ src/controllers/cart-controller.js
@@ -1,11 +1,13 @@
 const Cart = require('../models/cart-model');
 const skuChecker = require('../middlewares/skuChecker');
 
+
 const getCartBySessionId = async (req, res) => {
     try {
         let cart = null;
         const isExistingCart = await Cart.exists({_id: req.params.id}); //true se o carrinho já existir na collection
         console.log("[API]: O CARRINHO " + req.params.id + " EXISTE?", isExistingCart);
+
         if(!isExistingCart) {
             cart = await createCart(req.params.id);
         } else {
@@ -37,92 +39,73 @@ const createCart = async (id) => {
     return savedCart;
 }
 
-
-const addDummyCart = async (req, res) => {
-    const cart = new Cart({
-        _id: "tijolo",
-        items: req.body.items
-    });
-    try {
-        const savedCart = await cart.save();
-        res.json(savedCart);
-        console.log(savedCart)
-    } catch (error) {
-        res.status(400).send({"error": error.message});
-    }
-}
-
-
+/* DELETE A PRODUCT CART BY GET ID (Eg.: [GET] localhost/api/cart/{cartId}/{skuId} ) */
 const getCartProduct = async (req, res) => {
     try {
         await Cart.findOne({_id: req.params.id}).then((result) => {
-        let singleProduct = result.items.filter(product => {
-                if(product.SKU == req.params.item)
-                {
-                    return product;
-                }
+            const singleProduct = result.items.find(item => item.SKU === req.params.item);
+            singleProduct ? 
+                res.json(singleProduct) : 
+                res.status(404).send({"error": `The product with SKU ${req.params.item} was not found on this cart`});
         });
-        if(Array.isArray(singleProduct) && singleProduct.length) {
-            res.json(singleProduct);
-        } else {
-            res.status(404).send({"error": `The product with SKU ${req.params.item} was not found on this cart`});
-        }
-        
-        })
     } catch (error) {
         res.status(400).send({"error": error.message});
     }
-    
 }
 
+/* DELETE A PRODUCT CART BY GET ID (Eg.: [DELETE] localhost/api/cart/{cartId}/{skuId} ) */
 const deleteProductFromCart = async (req, res) => {
     await Cart.findOneAndUpdate(
         { _id: req.params.id },
         { $pull: { items: { SKU: req.params.item } } },
-        { new: true }
-      )
+        { new: true })
         .then(result => res.json(result))
         .catch(err => console.log(err));
 }
 
+
+
+
 const addProductToCart = async (req, res) => {
     const skuOnProductList = await skuChecker.onTheProductList(req, res);
     const skuOnCart        = await skuChecker.onTheCart(req, res);
-    
-    console.log(skuOnProductList);
-    console.log(skuOnCart);
-    
-    if(skuOnProductList)
-    {
-        if(!skuOnCart) {
-            const newCartItem = {"SKU": skuOnProductList.id, "qty": req.body.qty, "unitValue": skuOnProductList.price};
 
+    if(skuOnProductList) {
+        if(skuOnCart) {
+            let newQty = skuOnCart.qty + req.body.qty;
+
+            if(newQty > skuOnProductList.inventory) {
+                newQty = skuOnProductList.inventory;
+            } else if (newQty <= 0) {
+                newQty = 1;
+            }
+
+
+            await Cart.findOne({_id: req.params.id}).then(cart => {
+                const objIndex = cart.items.findIndex(obj => obj.SKU === req.body.sku);
+                cart.items[objIndex].qty = newQty;
+                cart.save();
+                res.json(cart);
+            });
+
+       } else {
+            const newCartItem = {"SKU": skuOnProductList.id, "qty": req.body.qty, "unitValue": skuOnProductList.price};
             await Cart.findOne({_id: req.params.id}).then(cart => {
                 cart.items.push(newCartItem);
                 cart.save();
                 res.json(cart);
             });
-       } else {
-            const newQty = skuOnCart.qty + req.body.qty;
-            
-            await Cart.findOne({_id: req.params.id}).then(cart => {
-                const objIndex = cart.items.findIndex(obj => obj.SKU === req.body.sku);
-                console.log(objIndex);
 
-                cart.items[objIndex].qty = newQty;
-
-                cart.save();
-                res.json(cart);
-            });
-        }
+       }
     }
 }
+
+
 
 
 module.exports = {
     getCartBySessionId,
     deleteProductFromCart,
-    addDummyCart,
     getCartProduct,
     addProductToCart
 }
