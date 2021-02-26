01c75b786bca80f99940ef044be451702555d9e1
leodhb <henrikleod@gmail.com>
2021-02-26 18:34:22 -0300

    Parte do controller refatorada

--- src/controllers/cart-controller.js
+++ src/controllers/cart-controller.js
@@ -2,44 +2,31 @@ const Cart = require('../models/cart-model');
 const skuChecker = require('../middlewares/skuChecker');
 
 
+/* GETS AN EXISTING CART BY USER SESSION ID (It'll be created if not exists) */
 const getCartBySessionId = async (req, res) => {
+    let cart = null;
     try {
-        let cart = null;
-        const isExistingCart = await Cart.exists({_id: req.params.id}); //true se o carrinho já existir na collection
-        console.log("[API]: O CARRINHO " + req.params.id + " EXISTE?", isExistingCart);
-
-        if(!isExistingCart) {
-            cart = await createCart(req.params.id);
-        } else {
-            cart = await Cart.findById(req.params.id);
-        }
+        const isCartCrated = await Cart.exists({_id: req.params.id});
+        isCartCrated ? cart = await Cart.findById(req.params.id) : cart = await createCart(req.params.id);
         res.json(cart);
     } catch (error) {
-        res.status(400).send({"Erro ao tentar consultar carrinho:": error.message});
+        res.status(400).send({error: `[CART] Falha ao tentar acessar o carrinho. Erro: ${error.message}`});
     }
 }
 
-
-/* AUX FUNCTION */
+/* CREATE A NEW CART BASED ON USER SESSION ID (Function called by getCartBySessionId method) */
 const createCart = async (id) => {
-    console.log('[API]: entrou na criacao do carrinho com o id', id);
-    let savedCart = null;
-    const cart = new Cart({
-        _id: id,
-        items: []
-    });
-
+    let createdCart = null;
+    const cart = new Cart({ _id: id , items: [] });
     try {
-        savedCart = await cart.save();
-        console.log('[CART] Carrinho criado');
-        console.log(savedCart);
+        createdCart = await cart.save();
     } catch (error) {
-        return {error: "deu ruim"}
+        return {error: `[CART] Falha na criação do carrinho. Erro: ${error.message}`}
     }
-    return savedCart;
+    return createdCart;
 }
 
-/* DELETE A PRODUCT CART BY GET ID (Eg.: [GET] localhost/api/cart/{cartId}/{skuId} ) */
+/* GETS A PRODUCT FROM THE CART BY SKU ID (Eg.: [GET] localhost/api/cart/{cartId}/{skuId} ) */
 const getCartProduct = async (req, res) => {
     try {
         await Cart.findOne({_id: req.params.id}).then((result) => {
@@ -64,8 +51,6 @@ const deleteProductFromCart = async (req, res) => {
 }
 
 
-
-
 const addProductToCart = async (req, res) => {
     const skuOnProductList = await skuChecker.onTheProductList(req, res);
     const skuOnCart        = await skuChecker.onTheCart(req, res);
@@ -79,8 +64,6 @@ const addProductToCart = async (req, res) => {
             } else if (newQty <= 0) {
                 newQty = 1;
             }
-
-
             await Cart.findOne({_id: req.params.id}).then(cart => {
                 const objIndex = cart.items.findIndex(obj => obj.SKU === req.body.sku);
                 cart.items[objIndex].qty = newQty;
@@ -95,11 +78,9 @@ const addProductToCart = async (req, res) => {
                 cart.save();
                 res.json(cart);
             });
-
        }
     }
 }
-
 
 
 
