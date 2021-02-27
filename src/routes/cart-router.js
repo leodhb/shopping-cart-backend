b3c6dd0da1086e4ba0bc9595a3f355a4e9cb5f95
leodhb <henrikleod@gmail.com>
2021-02-27 00:19:57 -0300

    Divisão de responsabilidades do método addProductToCart

--- src/routes/cart-router.js
+++ src/routes/cart-router.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { getCartBySessionId, 
         deleteProductFromCart,
         getCartProduct, 
-        addProductToCart } = require('../controllers/cart-controller');
+        addProductToCart,
+        updateCartProduct } = require('../controllers/cart-controller');
 
 const router = express.Router();
 
 /* cart routes */
 router.get('/:id', getCartBySessionId);
 router.put('/:id', addProductToCart);
+router.patch('/:id', updateCartProduct);
 router.get('/:id/:item', getCartProduct);
 router.delete('/:id/:item', deleteProductFromCart);
 
