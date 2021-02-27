01c67c424c3138a94ad4f240d426afd6e03a4677
leodhb <henrikleod@gmail.com>
2021-02-27 00:15:39 -0300

    Configuração do JEST

---
+++ src/tests/setup.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+beforeAll(done => mongoose.connect(process.env.DB_TEST_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false } ,done));
+afterAll(done => {
+    // Closing the DB connection allows Jest to exit successfully.
+    mongoose.connection.close()
+    done()
+})
