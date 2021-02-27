12063394a7ec0966e477ccbfdfc812295c2694e4
leodhb <henrikleod@gmail.com>
2021-02-27 01:03:36 -0300

    Product test script finished

--- src/tests/products.test.js
+++ src/tests/products.test.js
@@ -2,42 +2,52 @@
  * @jest-environment node
  */
 
-
 const request = require('supertest')
 const app = require('../app');
 
 const template = {
-    "name": "Serum antiacne",
-    "image": "https://cdn.shopify.com/s/files/1/0074/3486/2639/products/sa-carrossel-SA-sozinho.jpg",
-    "sku": {
-        "id": "S-SAA-250",
+    "name": "My Sample Product",
+    "image": "https://my.dummy.url/to/store/sample-image.png",
+    "sku": [{
+        "id": "MY-PUBLISHED-SKU",
         "inventory": "15",
         "price": "59.90"
-    }
+    }]
 };
 
-const feature = '/products';
+const endpoint = '/products';
 
 describe('Testing /products endpoint', () => {
+
   test('Inserindo um produto', async () => {
-    const { text, statusCode } = await request(app).post(feature).send(template)
-    expect(statusCode).toBe(200)
+    const { text, statusCode } = await request(app).post(endpoint).send(template);
+    const response = JSON.stringify(text);
+    expect(statusCode).toBe(200);
+    expect(response).toContain('My Sample Product');
+    expect(response).toContain('my.dummy.url');
+    expect(response).toContain('MY-PUBLISHED-SKU');
+    expect(response).toContain('_id');
   });
 
-  const testWithoutProperty = async (property) => {
-    const product = { ...template };
-    delete product[property];
-    const { text, statusCode } = await request(app).post(feature).send(product);
-    const { error } = JSON.parse(text);
+  test('Tentando inserir produto com atributos faltando', async () => {
+    await testWithoutProperty('name', 'required');
+    await testWithoutProperty('image', 'required');
+    await testWithoutProperty('sku', 'required');
+  });
 
+  test('Inserindo produto sem SKU', async () => {
+    await testWithoutProperty('sku', 'at least one', true);
+  });
+
+  /* AUXILIARY METHODS */
+  const testWithoutProperty = async (property, required, isArray = false) => {
+    const product = { ...template };
+    isArray ? product[property] = [] : delete product[property];
+    const { text, statusCode } = await request(app).post(endpoint).send(product);
+    const { error } = JSON.parse(text);
     expect(statusCode).toBe(400);
     expect(error).toContain(property);
-    expect(error).toContain('required');
+    expect(error).toContain(required);
   }
-
-  test('Inserindo produto com atributo faltando', async () => {
-    await testWithoutProperty('image');
-  })
-
 
 });
