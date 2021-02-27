0313184852499e05df5ea4b9722114c682925b27
leodhb <henrikleod@gmail.com>
2021-02-27 00:18:14 -0300

    Adicionado um helper para a quantidade do item (0 > qty <= inventory)

---
+++ src/helpers/index.js
@@ -0,0 +1,12 @@
+const quantityHandler = (value, max) => {
+    if(value > max) {
+        value = max;
+    } else if (value <= 0) {
+        value = 1;
+    }
+    return value;
+}
+
+module.exports = {
+    quantityHandler
+}
