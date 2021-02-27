66f880545dbbb1d2658adb5359f315f30a701275
leodhb <henrikleod@gmail.com>
2021-02-27 12:59:21 -0300

    Refatorando Controller do carrinho

--- src/controllers/cart-controller.js
+++ src/controllers/cart-controller.js
@@ -9,10 +9,12 @@ const getCartBySessionId = async (req, res) => {
     let cart = null;
     try {
         const isCartCrated = await Cart.exists({_id: req.params.id});
-        isCartCrated ? cart = await Cart.findById(req.params.id) : cart = await createCart(req.params.id);
+        isCartCrated ? 
+            cart = await Cart.findById(req.params.id) : 
+            cart = await createCart(req.params.id);
         res.json(cart);
     } catch (error) {
-        res.status(400).send({error: `[CART] Falha ao tentar acessar o carrinho. Erro: ${error.message}`});
+        res.status(400).send({error: `[CART] Falha ao tentar acessar o carrinho: ${error.message}`});
     }
 }
 
@@ -20,15 +22,11 @@ const getCartBySessionId = async (req, res) => {
 const createCart = async (id) => {
     let createdCart = null;
     const cart = new Cart({ _id: id , items: [] });
-    try {
-        createdCart = await cart.save();
-    } catch (error) {
-        return {error: `[CART] Falha na criação do carrinho. Erro: ${error.message}`}
-    }
+    createdCart = await cart.save().catch(err => {res.status(400).send({error: `[CART] Erro ao tentar criar carrinho: ${err.message}`})});
     return createdCart;
 }
 
-/* GETS A PRODUCT FROM THE CART BY SKU ID (Eg.: [GET] localhost/api/cart/{cartId}/{skuId} ) */
+/* GETS A PRODUCT FROM THE CART BY SKU PARAM (Eg.: [GET] localhost/api/cart/{cartId}/{skuId} ) */
 const getCartProduct = async (req, res) => {
     try {
         await Cart.findOne({_id: req.params.id}).then((result) => {
@@ -42,16 +40,17 @@ const getCartProduct = async (req, res) => {
     }
 }
 
-/* DELETE A PRODUCT CART BY GET ID (Eg.: [DELETE] localhost/api/cart/{cartId}/{skuId} ) */
+/* DELETE A PRODUCT CART BY ID PARAM (Eg.: [DELETE] localhost/api/cart/{cartId}/{skuId} ) */
 const deleteProductFromCart = async (req, res) => {
     await Cart.findOneAndUpdate(
         { _id: req.params.id },
         { $pull: { items: { SKU: req.params.item } } },
         { new: true })
         .then(result => res.json(result))
-        .catch(err => console.log(err));
+        .catch(err => {res.status(400).send({"error": `[CART] Erro ao tentar deletar: ${err.message}`})});;
 }
 
+/* ADD PRODUCT TO CART BY PASSING SKU AND QTY {if already exists on cart it'll just increase/decrease the qty} */
 const addProductToCart = async (req, res) => {
     const skuOnProductList = await skuCheck.onProductList(req, res);
     const skuOnCart        = await skuCheck.onCart(req, res);
@@ -60,36 +59,33 @@ const addProductToCart = async (req, res) => {
         if(!skuOnCart) {
             let skuQuantity = quantityHandler(req.body.qty, skuOnProductList.inventory);
             const newCartItem = {"SKU": skuOnProductList.id, "qty": skuQuantity, "unitValue": skuOnProductList.price};
+
             await Cart.findOne({_id: req.params.id}).then(cart => {
                 cart.items.push(newCartItem);
                 cart.save();
                 res.json(cart);
-            });
+            }).catch(err => {res.status(400).send({"error": `[CART] Erro ao tentar atualizar: ${err.message}`})});;
        } else {
             updateCartProduct(req, res);
        }
     }
 }
 
-
+/* INCREASE OR DECREASE PRODUCT QTY ON CART {it'll be inserted if not exists on cart} */
 const updateCartProduct = async (req, res) => {
-
     const skuOnProductList = await skuCheck.onProductList(req, res);
     const skuOnCart        = await skuCheck.onCart(req, res);
 
     if(skuOnCart) {
-        try {
             let skuQuantity = quantityHandler((skuOnCart.qty + req.body.qty), skuOnProductList.inventory);
+
             await Cart.findOne({_id: req.params.id}).then(cart => {
                 const objIndex = cart.items.findIndex(obj => obj.SKU === req.body.sku);
                 cart.items[objIndex].qty = skuQuantity;
                 cart.save();
                 res.json(cart);
-            });
-        } catch (error) {
-            res.status(400).send({"error": `[CART] Erro ao atualizar o carrinho`});
-        }
-        
+            }).catch(err => {res.status(400).send({"error": `[CART] Erro ao tentar atualizar: ${err.message}`})});
+
     } else {
         addProductToCart(req, res);
     }
