863d4539af49a0622679a8fdeca280ff7cfbd826
leodhb <henrikleod@gmail.com>
2021-03-01 11:45:08 -0300

    Melhoria na abordagem do skuChecker

--- src/helpers/sku-checker.js
+++ src/helpers/sku-checker.js
@@ -1,43 +1,27 @@
 const Product = require('../models/product/product-model');
 const Cart = require('../models/cart/cart-model');
 
-const getSkuFromProductList = (req, res) => {
-    let singleSKU = false;
-    try {
-        if (!req.body.sku) throw new Error(`SKU was not defined`);
-
-        singleSKU = Product.findOne({ "sku.id": req.body.sku })
-            .then(result => {
-                if (!result) {
-                    res.status(404).send({ "error": `[SKUCHECKER] The product with SKU ${req.body.sku} was not found on the SKU list` });
-                    return false;
-                } else {
-                    return result.sku.find(sku => sku.id === req.body.sku)
-                }
-            });
-    } catch (error) {
-        res.status(400).send({ "error": `[SKUCHECKER] Error message: ${error.message}` });
+const getSkuFromProductList = (skuId) => {
+    if (typeof skuId === 'undefined') {
+        return false;
     }
+    let singleSKU = Product.findOne(
+        { 'sku.id': skuId},
+        { 'sku.$': 1 },
+        {new: true}
+    ).then(result => result ? result.sku[0] : false);
     return singleSKU;
 }
 
-const getSkuFromCart = (req, res) => {
-    let singleSKU = false;
-    try {
-        if (!req.body.sku) throw new Error(`SKU was not defined`);
-
-        singleSKU = Cart.findOne({ _id: req.params.id, "items.SKU": req.body.sku })
-            .then(result => {
-                if (!result) {
-                    return false;
-                } else {
-                    return result.items.find(item => item.SKU === req.body.sku)
-                }
-            });
-        return singleSKU;
-    } catch (error) {
-        res.status(400).send({ "error": `[SKUCHECKER] Error message: ${error.message}` });
+const getSkuFromCart = (skuId) => {
+    if (typeof skuId === 'undefined') {
+        return false;
     }
+    let singleSKU = Cart.findOne(
+        { 'items.SKU': skuId},
+        { 'items.$': 1 },
+        {new: true}
+    ).then(result => result ? result.items[0] : false);
     return singleSKU;
 }
 
@@ -47,8 +31,6 @@ const checkProductList = async (obj) => {
         SKUS = [...SKUS, sku.id];
     });
     const result = await Product.exists({ "sku.id": { $in: SKUS } });
-    console.log('SKU array: ', SKUS);
-    console.log('SKU exists on product list?', result);
     return result;
 }
 
