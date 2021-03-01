1320bb3b320b62fb8f1bef5badd3598416037097
leodhb <henrikleod@gmail.com>
2021-02-28 21:04:28 -0300

    Melhorando código do controller de produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,22 +1,13 @@
 const Product = require('../models/product/product-model');
 const { checkProductList } = require('../helpers/sku-checker');
 
-const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.json(products);
-    } catch (error) {
-        res.status(400).send({ "error": error });
-    }
+const getAllProducts = (req, res) => {
+    Product.find()
+    .then(result => res.json(result))
+    .catch(error => res.status(400).send({ "error": error }));
 }
 
 const insertProduct = async (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        sku: req.body.sku
-    });
-    
     try {
         let isSkuAlreadyRegistered = true;
 
@@ -30,7 +21,14 @@ const insertProduct = async (req, res) => {
             throw new Error('products validation failed: sku: The product must have at least one SKU');
         } else if(isSkuAlreadyRegistered) {
             throw new Error('1 or many SKUs is already in use');
-        } else { 
+        } else {
+
+            const product = new Product({
+                name: req.body.name,
+                image: req.body.image,
+                sku: req.body.sku
+            });
+            
             const savedProduct = await product.save();
             res.json(savedProduct);
         }
@@ -39,40 +37,37 @@ const insertProduct = async (req, res) => {
     }
 }
 
-const getProductById = async (req, res) => {
-    try {
-        const singleProduct = await Product.findById(req.params.id);
-        res.json(singleProduct);
-    } catch (error) {
-        res.status(400).send({ "error": error });
-    }
+const getProductById = (req, res) => {
+    Product.findById(req.params.id)
+        .then(result => res.json(result))
+        .catch(error => res.status(400).send({ "error": error }));
+
 }
 
-const deleteProduct = async (req, res) => {
-    try {
-        const deletedProduct = await Product.remove({ _id: req.params.id });
-        res.json(deletedProduct);
-    } catch (error) {
-        res.status(400).send({ "error": error });
-    }
+const deleteProduct = (req, res) => {
+    Product.findOneAndDelete({ _id: req.params.id }, {new: true})
+        .then(result => res.json(result))
+        .catch(error => res.status(400).send({ "error": error }));
 }
 
-const updateProduct = async (req, res) => {
-    try {
-        const updatedProduct = await Product.updateOne({ _id: req.params.id }, { $set: { name: req.body.name, image: req.body.image, sku: [req.body.sku] } });
-        res.json(updatedProduct);
-    } catch (error) {
-        res.status(400).send({ "error": error });
-    }
+const updateProduct = (req, res) => {
+    Product.findOneAndUpdate(
+        { "_id": req.params.id }, 
+        { "$set": 
+            { "name": req.body.name, 
+              "image": req.body.image, 
+              "sku": [req.body.sku] 
+            } 
+        },
+        {new: true})
+        .then(result => res.json(result))
+        .catch(error => res.status(400).send({ "error": error }));
 }
-
-const findOnProducts = Product.findOne;
 
 module.exports = {
     getAllProducts,
     insertProduct,
     getProductById,
     deleteProduct,
-    updateProduct,
-    findOnProducts
+    updateProduct
 }
